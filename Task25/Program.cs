e6d605b285916a40ad44675f5cb85d8abda1545d
Димка я <  @mail.ru>
2022-10-05 18:01:02 +0300

    task 25 cikl prinimaet two chisla

---
+++ Task25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 1-е число");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число");
+int b =Convert.ToInt32(Console.ReadLine());
+int result  =1;
+for (int i =1;i<=b;i++)
+{
+    result = result *a;
+}
+Console.WriteLine(result);
